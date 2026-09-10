1ae1c17b15da04cd3008d643f40ad71eb4c4cb33
Selling-god <wezxcw1872@gmail.com>
2026-09-10 16:24:28 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,8 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.map((key) => caches.delete(key)));
-    await self.registration.unregister();
-    const pages = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
-    for (const page of pages) page.navigate(page.url);
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    try{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));}catch{}
+    try{await self.registration.unregister();}catch{}
+    try{const pages=await self.clients.matchAll({type:'window',includeUncontrolled:true});for(const page of pages)page.navigate(page.url);}catch{}
   })());
 });
